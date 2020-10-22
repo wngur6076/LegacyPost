95fa675bb48b813cc055625204c6206f57f5f0b0
wngur6076 <wngur6076@naver.com>
2020-10-22 17:45:09 +0900

    php postIndex

--- app.js
+++ app.js
@@ -13,3 +13,22 @@ if ($editor instanceof HTMLElement) {
         })
     });
 }
+
+/**
+ * Readmore
+ */
+const $readmore = document.getElementById('readmore')
+if ($readmore instanceof HTMLElement) {
+  let page = 0
+  $readmore.addEventListener('click', () => fetch('/?page=' + ++page,
+   { method: 'get' }).then(async response => {
+    const parser = new DOMParser()
+    const doc = parser.parseFromString(await response.text(), 'text/html')
+    const list = doc.querySelectorAll('.uk-container > .uk-list > li')
+    if (list.length > 0) {
+      Array.from(list).forEach(item => {
+        document.querySelector('.uk-container .uk-list').appendChild(item)
+      })
+    }
+  }))
+}
